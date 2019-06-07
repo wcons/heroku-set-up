de6abeae8d35c8e3787a64fe7bd8d84237700b54
wcons <wconstantino@gmail.com>
2019-06-07 15:18:55 -0400

    Add Books DB
    Add Books DB and initialize with data

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :books, except: %i[new edit]
   resources :things
   # RESTful routes
   resources :examples, except: %i[new edit]
